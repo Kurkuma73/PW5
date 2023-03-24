f47c3a7930ca1a5df58e2c796f62d66091db479f
Kurkuma73 <123757383+Kurkuma73@users.noreply.github.com>
2023-03-24 22:40:44 +0300

    Add files via upload

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace PW5
+{
+    interface IHello
+    {
+        void SayHello();
+    }
+
+    class GermanHello : IHello
+    {
+        public void SayHello()
+        {
+            Console.WriteLine("Guten Tag");
+        }
+    }
+    class EnglishHello : IHello
+    {
+        public void SayHello()
+        {
+            Console.WriteLine("Hello");
+        }
+    }
+    class LuxembourgishHello : IHello
+    {
+        public void SayHello()
+        {
+            Console.WriteLine("Merci");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<IHello> hellos = new List<IHello>();
+            hellos.Add(new GermanHello());
+            hellos.Add(new EnglishHello());
+            hellos.Add(new LuxembourgishHello());
+
+            foreach (var hello in hellos)
+            {
+                SayHello(hello);
+            }
+
+            Console.ReadKey();
+        }
+        static void SayHello(IHello hello)
+        {
+            hello.SayHello();
+        }
+    }
+}
---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace PW5
+{
+    interface IFilter
+    {
+        string Execute(string textLine);
+    }
+    class DigitFilter : IFilter
+    {
+        public string Execute(string textLine)
+        {
+            string text = "";
+
+            foreach (char c in textLine)
+            {
+                if (char.IsLetter(c))
+                {
+                    text += c;
+                }
+            }
+
+            return text;
+        }
+    }
+    class LetterFilter : IFilter
+    {
+        public string Execute(string textLine)
+        {
+            string text = "";
+
+            foreach (char c in textLine)
+            {
+                if (char.IsDigit(c))
+                {
+                    text += c;
+                }
+            }
+
+            return text;
+        }
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IFilter digitFilter = new DigitFilter();
+            IFilter letterFilter = new LetterFilter();
+
+            string text = "ялюб434илакот44837енкааоннел38738юбилменя";
+
+            Console.WriteLine($"Текст: {text}");
+            Console.WriteLine($"Текст после использования DigitFilter: {digitFilter.Execute(text)}");
+            Console.WriteLine($"Текст после использования LetterFilter: {letterFilter.Execute(text)}");
+
+            Console.ReadKey();
+        }
+    }
+}
+
